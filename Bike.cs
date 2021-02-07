f6b73ee67d13d1401861595d83196cadec72a761
Joseph Erxleben <jerxleben.13@gmail.com>
2021-02-06 22:17:13 -0500

    Removed test class because it wasn't working, instituted Bike attributes and methods. Began writing tutorial.

--- Bike.cs
+++ Bike.cs
@@ -7,7 +7,54 @@ namespace Choose_Your_Class
     public class Bike
     {
 
+        public string Type { get; set; }
+        public string Color { get; set; }
+        public int Durability { get; set; } = 100;
+        public bool IsRented { get; set; } = false;
+        public int RentalTimeRemaining { get; set; } = 0;
 
+        public string GetColor()
+        {
+            string color = Color;
+            return color;
+        }
+
+        public Bike()
+        {
+            
+        }
+
+        public Bike(string type, string color)
+        {
+            Type = type;
+            Color = color;
+        }
+
+        public void PerformMaintenance()
+        {
+            Durability += 20;
+            if (Durability > 100)
+            {
+                Durability = 0;
+            }
+        }
+
+        public void RentBike()
+        {
+            IsRented = true;
+            RentalTimeRemaining = 4;
+            Durability -= 30;
+        }
+
+        public void Tick()
+        {
+            RentalTimeRemaining -= 1;
+            if (RentalTimeRemaining <= 0)
+            {
+                IsRented = false;
+                RentalTimeRemaining = 0;
+            }
+        }
 
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,5 +6,35 @@ namespace Choose_Your_Class
 {
     class Program
     {
+        static void Main()
+        {
+
+            List<Bike> listOfBikes = new List<Bike>();
+            Random random = new Random();
+            List<string> daysOfWeek = new List<string>() 
+                {
+                "Sunday", "Monday", "Teusday", "Wednesday", "Thursday", "Friday", "Saturday"
+                };
+
+            string shopName = "Bike Rentals R' US";
+            int funds = 1000;
+
+
+            bool keepPlaying = false;
+
+            Console.WriteLine($"Welcome, new managaer of {shopName}!");
+            Console.WriteLine("This onboarding experience is designed to help you suceed in your new position!");
+            Console.WriteLine("When you are ready, press any key to continue.");
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+        }
+
     }
 }
